f9a5b73860dcfccd125ade57a81a1ad25c4939f1
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2022-09-02 23:49:19 -0500

    Pagina Inicio, Algunas carpetas.
    Creacion de algunas carpetas para tener más orden.

---
+++ proyecto/src/components/paginaprincipal/PaginaPrincipal.js
@@ -0,0 +1,24 @@
+import './PaginaPrincipal.css'
+
+export const PaginaPrincipal = () => {
+    return (
+        <>
+            <div id='centrar'>
+                <img id='img01' className='center-block'></img>
+            </div>
+            <div id='centro'>
+                <div id='cir01'></div>
+                <div id='cir02'></div>
+                <div id='cir03'></div>
+
+                <p id='mensaje'> ¿HAMBRE? <br />
+                    EN PANCE SE <br />
+                    LO LLEVAMOS
+                </p>
+            </div>
+            <h1 id='centrar'>
+                Promociones del Día
+            </h1>
+        </>
+    )
+}
