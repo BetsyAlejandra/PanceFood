f5007412fc7dab1c69431056b9ad07c008fa8457
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2022-10-02 17:36:11 -0500

    icono y fuentes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { PaginaPrincipal } from './components/paginaprincipal/PaginaPrincipal';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PaginaPrincipal />
+
   </React.StrictMode>
 );
 
