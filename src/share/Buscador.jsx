9e7f7c13f5dcb1afd99af715fe28b2efa13252a2
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2022-10-04 16:41:47 -0500

    Buscador

--- src/share/Buscador.jsx
+++ src/share/Buscador.jsx
@@ -1,10 +1,17 @@
 import './Buscador.css'
+import search from '../icons/search.png'
 
 export const Buscador = () => {
+
+    
+
     return (
         <body>
             <div id='centrar'>
-                <div id='buscador'></div>
+                <div id='buscador'>
+                    <img id='img' src={search} alt='buscar' />
+                    <input id='buscar' type='search'></input>
+                </div>
             </div>
         </body>
     )
